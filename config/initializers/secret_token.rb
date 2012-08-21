812def961511e7c9abadf1e558d30888213e6ecf
Jefferson Carley <jeff.carley@gmail.com>
2012-08-21 10:47:38 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeffersoncarley::Application.config.secret_token = '8ed2d7aac62304c4671f5c014e1beb539516dd6fdf0bf4d0cbe892f43af3918e482b56f06f78a59876fb8ec4df66fe32720c4dccfd2040fd333c1dd5a6045b60'
