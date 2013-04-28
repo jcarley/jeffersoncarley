2453ffca31b49df2f3a64f6789f6c84ecea0fcf6
jcarley <jcarley@gettyimages.com>
2013-04-28 10:02:56 -0500

    Filters out sample files in production.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,25 @@
 class HomeController < ApplicationController
 
+  EXTENSION_REGEX = /\.md$|\.markdown$/
+  SAMPLEFILE_REGEX = /sample\..+[\.md$|\.markdown$]/
+
   def index
     files = Dir.entries(File.join(Rails.root, "app/views/articles"))
     posts = files.map do |x|
-      Post.new(x) if x.index(/\.md$|\.markdown$/).present?
+      Post.new(x) if include?(x)
     end
     posts.compact!
 
     @decorator = PostDecorator.decorate(posts)
+  end
+
+  private
+
+  def include?(filename)
+    if Rails.env.production?
+      filename.index(EXTENSION_REGEX).present? && !filename.index(SAMPLEFILE_REGEX).present?
+    else
+      filename.index(EXTENSION_REGEX).present?
+    end
   end
 end
